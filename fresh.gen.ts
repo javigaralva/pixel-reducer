fbe0d09f9bd4341b565c88b02e14c4b5444e0779
javigaralva <javigaralva@gmail.com>
2023-03-04 00:01:16 +0100

    fix: routes

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,22 +3,16 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import config from './deno.json' assert { type: 'json' }
-import * as $0 from './routes/[name].tsx'
-import * as $1 from './routes/api/joke.ts'
-import * as $2 from './routes/api/optimize/[url].ts'
-import * as $3 from './routes/api/zip.ts'
-import * as $4 from './routes/index.tsx'
-import * as $5 from './routes/optimize/[url].tsx'
+import * as $0 from './routes/api/optimize/[url].ts'
+import * as $1 from './routes/api/zip.ts'
+import * as $2 from './routes/index.tsx'
 import * as $$0 from './islands/App.tsx'
 
 const manifest = {
     routes: {
-        './routes/[name].tsx': $0,
-        './routes/api/joke.ts': $1,
-        './routes/api/optimize/[url].ts': $2,
-        './routes/api/zip.ts': $3,
-        './routes/index.tsx': $4,
-        './routes/optimize/[url].tsx': $5,
+        './routes/api/optimize/[url].ts': $0,
+        './routes/api/zip.ts': $1,
+        './routes/index.tsx': $2,
     },
     islands: {
         './islands/App.tsx': $$0,
