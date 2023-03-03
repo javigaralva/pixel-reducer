745ed1f2fa3c556ff0677b72ba823e9e936db71c
javigaralva <javigaralva@gmail.com>
2023-03-04 00:49:25 +0100

    fix: linter error

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,5 +1,5 @@
 import { asset, Head } from '$fresh/runtime.ts'
-import App from "/islands/App.tsx"
+import App from '/islands/App.tsx'
 
 export default function Home() {
     return (
@@ -7,7 +7,7 @@ export default function Home() {
             <Head>
                 <title>Pixel Reducer</title>
                 <link rel='preconnect' href='https://fonts.googleapis.com' />
-                <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
+                <link rel='preconnect' href='https://fonts.gstatic.com' />
                 <link
                     href='https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700&display=swap'
                     rel='stylesheet'
