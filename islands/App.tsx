486b597be5edfd4d37b380db2b1a11a2a3b2eff8
javigaralva <javigaralva@gmail.com>
2023-03-05 19:14:16 +0100

    feat: add movement to brand title

--- islands/App.tsx
+++ islands/App.tsx
@@ -79,7 +79,11 @@ function App() {
     return (
         (<div className='App'>
             <header className='brand'>
-                <h1 className='brand__title'><span className={`brand__title_pixel ${expandedTitleClass}`}>Pixel</span><span className={`brand__title_reducer ${expandedTitleClass}`}>Reducer</span></h1>
+                <h1 className='brand__title'>
+                    <span className={`brand__title_pixel ${expandedTitleClass}`}>Pixel</span>
+                    <br/>
+                    <span className={`brand__title_reducer ${expandedTitleClass}`}>Reducer</span>
+                </h1>
                 <h2 className='brand__subtitle'>Optimizes the size of images on a website.</h2>
                 <p className='brand__description'>Download all images with one click. Commitment to quality with zero setup.</p>
             </header>
