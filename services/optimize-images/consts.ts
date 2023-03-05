e05ac8bb5a0486899d989c167755ec53fb9dee0c
javigaralva <javigaralva@gmail.com>
2023-03-05 23:05:27 +0100

    refactor: extract subpaths

--- services/optimize-images/consts.ts
+++ services/optimize-images/consts.ts
@@ -13,7 +13,9 @@ export type ImageContentType = keyof typeof IMAGE_CONTENT_TYPES_EXTENSIONS
 export const IMAGE_CONTENT_TYPES = Object.keys(IMAGE_CONTENT_TYPES_EXTENSIONS)
 export const IMAGE_EXTENSIONS = Object.values(IMAGE_CONTENT_TYPES_EXTENSIONS)
 
-const cloudinaryProductEnvironment = Deno.env.get('CLOUDINARY_PRODUCT_ENVIRONMENT') ?? 'demo'
+const CLOUDINARY_BASE_URL = `https://res.cloudinary.com`
+const CLOUDINARY_PRODUCT_ENVIRONMENT = Deno.env.get('CLOUDINARY_PRODUCT_ENVIRONMENT') ?? 'demo'
+const CLOUDINARY_IMAGE_FETCH_URL = `${CLOUDINARY_BASE_URL}/${CLOUDINARY_PRODUCT_ENVIRONMENT}/image/fetch`
 
-export const CLOUDINARY_OPTIMIZED_URL = `https://res.cloudinary.com/${cloudinaryProductEnvironment}/image/fetch/q_auto:low`
-export const CLOUDINARY_THUMBNAIL_URL = `https://res.cloudinary.com/${cloudinaryProductEnvironment}/image/fetch/c_scale,c_lfill,w_200,q_auto:low`
+export const CLOUDINARY_OPTIMIZED_URL = `${CLOUDINARY_IMAGE_FETCH_URL}/q_auto:low`
+export const CLOUDINARY_THUMBNAIL_URL = `${CLOUDINARY_IMAGE_FETCH_URL}/c_scale,c_lfill,w_200,q_auto:low`
