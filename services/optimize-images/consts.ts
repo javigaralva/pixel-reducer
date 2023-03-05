89470ba67ca59298bc1a75a0a36323ba038b3fac
javigaralva <javigaralva@gmail.com>
2023-03-05 22:59:59 +0100

    feat: add CLOUDINARY_PRODUCT_ENVIRONMENT variable

--- services/optimize-images/consts.ts
+++ services/optimize-images/consts.ts
@@ -13,5 +13,7 @@ export type ImageContentType = keyof typeof IMAGE_CONTENT_TYPES_EXTENSIONS
 export const IMAGE_CONTENT_TYPES = Object.keys(IMAGE_CONTENT_TYPES_EXTENSIONS)
 export const IMAGE_EXTENSIONS = Object.values(IMAGE_CONTENT_TYPES_EXTENSIONS)
 
-export const CLOUDINARY_OPTIMIZED_URL = 'https://res.cloudinary.com/demo/image/fetch/q_auto:low'
-export const CLOUDINARY_THUMBNAIL_URL = 'https://res.cloudinary.com/demo/image/fetch/c_scale,c_lfill,w_200,q_auto:low'
+const cloudinaryProductEnvironment = Deno.env.get('CLOUDINARY_PRODUCT_ENVIRONMENT') ?? 'demo'
+
+export const CLOUDINARY_OPTIMIZED_URL = `https://res.cloudinary.com/${cloudinaryProductEnvironment}/image/fetch/q_auto:low`
+export const CLOUDINARY_THUMBNAIL_URL = `https://res.cloudinary.com/${cloudinaryProductEnvironment}/image/fetch/c_scale,c_lfill,w_200,q_auto:low`
