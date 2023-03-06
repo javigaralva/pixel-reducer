3fb920d31997d1c2847658296c879c508afefa52
javigaralva <javigaralva@gmail.com>
2023-03-06 18:23:57 +0100

    fix: catch error on fetch

--- services/optimize-images/helpers/getWebsiteInfo.ts
+++ services/optimize-images/helpers/getWebsiteInfo.ts
@@ -1,18 +1,25 @@
 import { cheerio } from 'https://deno.land/x/denocheerio@1.0.0/mod.ts'
 
 export async function getWebsiteInfo({ url }: { url: string }) {
-    const rawHtml = await fetch(url).then((response) => response.text())
-    const $ = cheerio.load(rawHtml)
-    const allImagesUrls = [
-        ...$('img').map((_i, e) => {
-            const src = $(e).attr('src') ?? ''
-            return src ? new URL(src, url).toJSON() : ''
-        }),
-    ]
-    const imagesUrls = [...new Set(allImagesUrls)].filter(Boolean)
-    const description = $('meta[name="description"]').attr('content') ?? ''
-    return {
-        description,
-        imagesUrls,
+    try {
+        const rawHtml = await fetch(url).then((response) => response.text())
+        const $ = cheerio.load(rawHtml)
+        const allImagesUrls = [
+            ...$('img').map((_i, e) => {
+                const src = $(e).attr('src') ?? ''
+                return src ? new URL(src, url).toJSON() : ''
+            }),
+        ]
+        const imagesUrls = [...new Set(allImagesUrls)].filter(Boolean)
+        const description = $('meta[name="description"]').attr('content') ?? ''
+        return {
+            description,
+            imagesUrls,
+        }
+    } catch (_error) {
+        return {
+            description: '',
+            imagesUrls: [],
+        }
     }
 }
